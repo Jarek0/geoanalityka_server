d0f86cdd789d1ae3f8fcfb703ed54373d99a28c1
jmalec <jmalec@gis-expert.pl>
2016-07-01 14:21:21 +0200

    Zmiana startowej liczby kredytów.

--- geoanalityka-cms-ejb/src/main/java/pl/gisexpert/cms/model/Account.java
+++ geoanalityka-cms-ejb/src/main/java/pl/gisexpert/cms/model/Account.java
@@ -79,7 +79,7 @@ public class Account implements Serializable {
     private AccountStatus accountStatus;
     
     @Column(nullable = false)
-    private Double credits = 99999.0;
+    private Double credits = 100.0;
 
     @Embedded
     private ResetPassword resetPassword;
